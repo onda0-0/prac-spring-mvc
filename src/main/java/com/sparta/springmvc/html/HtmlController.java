f81fea43cdf4c5c7d413cfda67a80c47f034cece
onda0-0 <qwen0524@gmail.com>
2024-05-14 20:06:27 +0900

    동적페이지 1-11

--- src/main/java/com/sparta/springmvc/html/HtmlController.java
+++ src/main/java/com/sparta/springmvc/html/HtmlController.java
@@ -1,10 +1,12 @@
 package com.sparta.springmvc.html;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HtmlController {
+    private static long visitCount=0;
     //1. url 경로직접접근
     //2. thymeleaf 안쓰고 한번 컨트롤러 통해 반환
     //3. thymeleaf쓰고 컨트롤러 통해 반환
@@ -39,5 +41,16 @@ public class HtmlController {
         //정적인 파일도 templates에 넣어도 된다.
         //tempaltes폴더에있는 html파일이 반환된다. 타임리프 엔진을 적용해서 html을 반환할때에는 이름뒤에html을 떼고 이름만 적어도 된다.
         //tempaltes폴더에있는 html파일을 찾을때는 파일의 이름만 반환한다. 컨트롤러 어노테이션이 있는 클래스에서! -14분28초
+        //controller라는 annotation이 달린 클래스에서 templates에 들어 있는 파일의 이름을 문자열로 받아와서 반환할수 있게 된다.
     }
+
+    @GetMapping("/html/dynamic")
+    public String htmlDynamic(Model model){
+        visitCount++;
+        model.addAttribute("visits",visitCount);
+        //식별되는 이름을 넣어주고, 데이터를 넣어준다.
+        //동적페이지를 만들때 필요한건 두가지: 1. 모델 2. 뷰이름 정보
+        return "hello-visit";
+
+    }//반환할 데이터는 모델에 담고, 그 모델에 적용이 될 뷰네임의 정보를 전달하면 viewreasable?을 통해서 그모델의 데이터가 뷰에 적용이되고 완성된 화면이 클라이언트에게 전달된다.
 }
